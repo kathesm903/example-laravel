11c9e6b38272ec3d49d4aa76e30c92dbf78d03fc
Katherine <kandrade@messangi.com>
2022-07-11 18:06:44 -0500

    creando primera ruta a client

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container py-5 text-center">
     <h1>Hola Mundo!</h1>
-    <a href="#" class="btn btn-primary">Clientes</a>
+    <a href="{{ route('client.index') }}" class="btn btn-primary">Clientes</a>
 </div>
 
 @endsection
